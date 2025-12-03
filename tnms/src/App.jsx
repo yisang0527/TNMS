da64a4ecdd261b5c2d2af03e68390660cb4d4a3f
jeongwon456 <jeongwon545@gmail.com>
2025-12-03 15:41:44 +0900

    feat: 산사태산불제외

--- tnms/src/App.jsx
+++ tnms/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import StatsChart from './component/stats'
 
 function App() {
-
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path='/stats' element={<StatsChart />} />
+        </Routes>
+      </Router>
     </>
   )
 }
