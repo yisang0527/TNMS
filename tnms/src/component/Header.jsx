a999a28d7b8117496e6c9361657d2c1ce03e49ea
yoonseo0801 <167054560+yoonseo0801@users.noreply.github.com>
2025-12-04 14:33:36 +0900

    feat : 헤더 메뉴 완성

--- tnms/src/component/Header.jsx
+++ tnms/src/component/Header.jsx
@@ -4,10 +4,11 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
   const menuLst = [
-    { name: "지난재난통계", link: "/", subMenu: [{name:"지난재난통계",link:"/"}] },
-    { name: "재난예방대비", link: "/", subMenu: [{name:"재난대비행동요령",link:"/"}, {name:"비상연락망",link:"/"}] },
-    { name: "재난심리상담", link:"/", subMenu: [{name: "상담센터소개",link:"/"},{name:"재난심리상담",link:"/"},{name:"재난심리 자가진단",link:"/"}] },
-    { name: "참여와신고", link:"/", subMenu: [{name:"Q&A",link:"/"},{name:"재난신고하기",link:"/"},{name:"공지사항",link:"/"}] }
+    { name: "지난재난통계", subMenu: [{name:"지난재난통계",link:"/"}] },
+    { name: "재난예방대비", subMenu: [{name:"재난대비행동요령",link:"/"}, {name:"비상연락망",link:"/"}] },
+    { name: "재난심리상담", subMenu: [{name: "상담센터소개",link:"/"},{name:"재난심리상담",link:"https://www.redcross.or.kr/recovery/recovery_support.do",external: true},
+      {name:"재난심리 자가진단",link:"https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/pcm/cyb/SelfDgnssForm.jsp?menuSeq=566",external: true}] },
+    { name: "참여와신고", subMenu: [{name:"Q&A",link:"/"},{name:"재난신고하기",link:"https://www.safetyreport.go.kr/#main",external: true},{name:"공지사항",link:"/"}] }
   ];
 
   // 현재 호버 중인 메뉴 이름
@@ -24,14 +25,7 @@ const Header = () => {
             onMouseEnter={() => setHoverMenu(v.name)}
             onMouseLeave={() => setHoverMenu(null)}
           >
-            <v.name === "재난심리상담" ? (
-              <a href="https://www.redcross.or.kr/recovery/recovery_support.do" target="_blank" rel="noopener noreferrer">
-                {v.name}
-              </a>
-            ) : ( 
-
-            )
-            <Link to={v.link}>{v.name}</Link>
+           {v.name}
           </li>
         ))}
       </ul>
@@ -46,8 +40,8 @@ const Header = () => {
           >
             {v.subMenu.map((sub, subIdx) => (
               <li key={subIdx}>
-                <Link to={sub.link}>{sub.name}</Link>
-                </li>
+               <Link to={sub.link}>{sub.name}</Link>
+              </li>
             ))}
           </ul>
         ))}
