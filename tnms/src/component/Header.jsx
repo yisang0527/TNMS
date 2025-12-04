68fbc6fdbc7c836b2c00d45e17bebc9f39bd5b86
Yisang <yisang0527@gmail.com>
2025-12-04 11:33:05 +0900

    feat/ 리펙토링

--- tnms/src/component/Header.jsx
+++ tnms/src/component/Header.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import './Header.css';
 
 const Header = () => {
   const menuLst = [
     { name: "지난재난통계", subMenu: ["지난재난통계"] },
     { name: "재난예방대비", subMenu: ["재난대비행동요령", "비상연락망"] },
-    { name: "재난심리상담", subMenu: ["상담센터소개","재난심리상담","재난심리 자가진단"] },
-    { name: "참여와신고", subMenu: ["Q&A","재난신고하기","공지사항"] }
+    { name: "재난심리상담", subMenu: ["상담센터소개", "재난심리상담", "재난심리 자가진단"] },
+    { name: "참여와신고", subMenu: ["Q&A", "재난신고하기", "공지사항"] }
   ];
 
   // 현재 호버 중인 메뉴 이름
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <nav className="nav">
-        <img src="./logo1.png" alt="Logo" className="logo"/>
+      <img src="./logo1.png" alt="Logo" className="logo" />
       <ul className="navContainer">
         {menuLst.map((v, idx) => (
           <li
--- tnms/src/page/main.jsx
+++ tnms/src/page/main.jsx
@@ -1,7 +1,9 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function(){
-    return(
+export default function main() {
+    return (
+        <>
         
+        </>
     )
 }
