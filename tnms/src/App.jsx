d1b568d425bba6cbc1e1981c139ac2d14c50c490
yoonseo0801 <167054560+yoonseo0801@users.noreply.github.com>
2025-12-05 17:51:59 +0900

    feat:푸터 완성

--- tnms/src/App.jsx
+++ tnms/src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
-import Header from './component/Header'
-import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './component/HeaderComponent/Header';
+import Prev from './page/Prev';
+import Footer from './component/FooterComponent/Footer'
+import Number from './page/Number';
 
 function App() {
   return (
     <>
       <Header />
+
+      <Routes>
+        {/* 이곳에 link 주소를 라우트, path와 element로 넣으면 됩니다. */}
+
+        <Route path='/prev' element={<Prev />} />
+        <Route path='/number' element={<Number/>}/>
+      </Routes>
+
+      <Footer />
     </>
   )
 }
