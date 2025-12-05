214fa60977702e60e076cf603e8a089642c45af9
yoonseo0801 <167054560+yoonseo0801@users.noreply.github.com>
2025-12-05 18:08:06 +0900

    feat: 리팩토링

--- tnms/src/App.jsx
+++ tnms/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './component/HeaderComponent/Header';
 import Prev from './page/Prev';
+import Qna from './page/Qna';
 import Footer from './component/FooterComponent/Footer'
-import Number from './page/Number';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         {/* 이곳에 link 주소를 라우트, path와 element로 넣으면 됩니다. */}
 
         <Route path='/prev' element={<Prev />} />
-        <Route path='/number' element={<Number/>}/>
+        <Route path='/qna' element={<Qna />} />
       </Routes>
 
       <Footer />
